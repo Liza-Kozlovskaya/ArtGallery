8c34f56b3eeeb48321d669e08071523781ec0187
Liza-Kozlovskaya <liza.kozlovskaya.2011@mail.ru>
2019-02-24 21:42:17 +0300

    add paging

--- ArtGallery/WebUI/Controllers/ArtsController.cs
+++ ArtGallery/WebUI/Controllers/ArtsController.cs
@@ -4,20 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class ArtsController : Controller
     {
         private IArtRepository repository;
+        public int pageSize = 5;
         public ArtsController(IArtRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Arts);
+            //разбиение по количеству страниц на 1 странице
+            ArtsListViewModel model = new ArtsListViewModel
+            {
+                Arts = repository.Arts
+                .OrderBy(art => art.ArtId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Arts.Count()
+                }
+            };
+            
+            return View(model);
         }
     }
 }
---
+++ ArtGallery/WebUI/Models/ArtsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class ArtsListViewModel
+    {
+        public IEnumerable<Art> Arts { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
