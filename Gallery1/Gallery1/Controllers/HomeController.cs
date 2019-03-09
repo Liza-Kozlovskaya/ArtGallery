e70d663c88ff7d9187a7590ee09473bc0b8f4fd1
Liza-Kozlovskaya <liza.kozlovskaya.2011@mail.ru>
2019-03-10 01:55:12 +0300

    Add paging and catalog-gallery

--- Gallery1/Gallery1/Controllers/HomeController.cs
+++ Gallery1/Gallery1/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Gallery1.Models;
+using PagedList;
+using PagedList.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -11,10 +13,14 @@ namespace Gallery1.Controllers
     public class HomeController : Controller
     {
         ArtContext db = new ArtContext();
-        public ActionResult Index()
+        public int pageSize = 12;
+
+        public ActionResult Index(int? page)
         {
             var arts = db.ArtWorks.Include(a => a.Author).Include(a => a.Type).Include(a=>a.School).Include(a=>a.Location.City.Country);
-            return View(arts.ToList());
+            return View(arts.ToList().ToPagedList(page ?? 1, pageSize));
+            //http://professorweb.ru/my/ASP_NET/gamestore/level1/1_4.php навигация
+            //http://professorweb.ru/my/ASP_NET/gamestore/level1/1_6.php каталог    
         }
 
         public ActionResult About()
